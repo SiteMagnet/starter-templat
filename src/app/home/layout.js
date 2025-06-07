270834739bc97831b1b66d662b16a3abe7b14484
Tyythedeveloper33 <webstertyler81@yahoo.com>
2025-06-07 17:09:34 -0400

    added seo to content.json

--- src/app/home/layout.js
+++ src/app/home/layout.js
@@ -1,7 +1,8 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "../globals.css";
 import Navigation from "../components/Navigation";
-
+import content from "../../../content.json";
+const seo = content.SEO;
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -13,16 +14,14 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata = {
-  title: "SiteMagnet - Transform Your Website into a High-Converting Lead Magnet",
-  description:
-    "SiteMagnet helps businesses optimize their websites to generate more leads and conversions. Discover how our tailored website strategies can grow your business and boost sales.",
-  keywords: "website optimization, lead generation, high-converting websites, website design, digital marketing, business growth, conversion strategies",
-  author: "SiteMagnet Team",
-  robots: "index, follow", // This ensures that search engines can index your page and follow links
+  title: seo.title,
+  description: seo.description,
+  robots: seo.robots, // This ensures that search engines can index your page and follow links
   og: {
-    title: "SiteMagnet - Transform Your Website into a High-Converting Lead Magnet", // Open Graph title for social media sharing
-    description:
-      "Optimize your website to convert visitors into clients. SiteMagnet specializes in building high-converting websites that help your business grow."}}
+    title: seo.openGraph.title, // Open Graph title for social media sharing
+    description: seo.openGraph.description,
+  },
+};
 
 export default function RootLayout({ children }) {
   return (
